497af0e9946d5ddce63d5222c7a1af9df7d4e203
Nik <nikammerlaan@gmail.com>
2023-12-07 23:08:20 -0800

    Remove unnecessary bits of day 8 solution

--- src/main/java/com/github/nikammerlaan/aoc/days/day08/Day08Solution.java
+++ src/main/java/com/github/nikammerlaan/aoc/days/day08/Day08Solution.java
@@ -4,7 +4,6 @@ import com.github.nikammerlaan.aoc.days.AbstractDaySolution;
 import org.apache.commons.math3.util.ArithmeticUtils;
 
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -12,10 +11,24 @@ public class Day08Solution extends AbstractDaySolution<Input> {
 
     @Override
     protected Object solvePart1(Input input) {
+        return solve(input, "AAA");
+    }
+
+    @Override
+    protected Object solvePart2(Input input) {
+        // This technically doesn't cover all valid cases, but works for the inputs provided.
+        return input.network().keySet().stream()
+            .filter(key -> key.endsWith("A"))
+            .mapToLong(key -> solve(input, key))
+            .reduce(ArithmeticUtils::lcm)
+            .orElseThrow();
+    }
+
+    private int solve(Input input, String start) {
         var instructions = input.instructions();
-        var key = "AAA";
+        var key = start;
         for(int i = 0; true; i++) {
-            if(key.equals("ZZZ")) {
+            if(key.endsWith("Z")) {
                 return i;
             }
 
@@ -25,42 +38,6 @@ public class Day08Solution extends AbstractDaySolution<Input> {
         }
     }
 
-    @Override
-    protected Object solvePart2(Input input) {
-        var cycles = input.network().keySet().stream()
-            .filter(s -> s.endsWith("A"))
-            .map(s -> getCycle(s, input))
-            .toList();
-
-        // I don't really have a grasp on _why_ this works, but it's fast and gets the answer
-        return cycles.stream()
-            .mapToLong(Cycle::offset)
-            .map(i -> i + 1)
-            .reduce(ArithmeticUtils::lcm)
-            .orElseThrow();
-    }
-
-    record Cycle(int length, int offset) {}
-    private Cycle getCycle(String start, Input input) {
-        var instructions = input.instructions();
-        var key = start;
-        record SeenKey(String key, int instruction) {}
-        var ending = -1;
-        var seen = new HashSet<>();
-        for(int i = 0; true; i++) {
-            if(key.endsWith("Z")) {
-                ending = i - 1;
-            }
-            var seenKey = new SeenKey(key, i % instructions.length);
-            if(!seen.add(seenKey)) {
-                return new Cycle(i - 1, ending);
-            }
-
-            var instruction = instructions[i % instructions.length];
-            var node = input.network().get(key);
-            key = instruction == 'L' ? node.left() : node.right();
-        }
-    }
 
     @Override
     protected Input parseInput(String rawInput) {
